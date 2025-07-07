4dfc282faea01ad247a5b3d5ba8522c5e519d4c4
Shiv Kant <shivkant639624@gmail.com>
2025-07-07 16:34:31 +0000

    oops completed

--- javascript/part6/oop-master.js
+++ javascript/part6/oop-master.js
@@ -14,8 +14,8 @@ function Person(name, age) {
 }
 
 let john = new Person("John Doe", 20);
-console.log(john.name);
-console.log(john);
+// console.log(john.name);
+// console.log(john);
 
 function Animal(type) {
   this.type = type;
@@ -30,7 +30,31 @@ Array.prototype.shiv = function () {
 };
 
 let myArray = [1, 2, 3];
-console.log(myArray.shiv());
+// console.log(myArray.shiv());
 
 let myNewArray = [1, 2, 3, 4, 5];
-console.log(myNewArray.shiv());
+// console.log(myNewArray.shiv());
+
+class Vehical {
+  constructor(model, make) {
+    this.model = model;
+    this.make = make;
+  }
+  start() {
+    return `${this.model} is a car from ${this.make}`;
+  }
+}
+
+class Car extends Vehical {
+  drive() {
+    return `${this.make} : This is an inheritance example`;
+  }
+}
+
+let myCar = new Car("Toyota", "Carolla");
+// console.log(myCar.start());
+// console.log(myCar.drive());
+
+let Vehical1 = new Vehical("Toyota", "Carolla");
+console.log(Vehical1.start());
+// console.log(Vehical1.drive());
