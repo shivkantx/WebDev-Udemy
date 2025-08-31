65430e40825b0918e2fd922265041df4554511b4
Shiv Kant <shivkant639624@gmail.com>
2025-08-31 22:42:44 +0530

    Update .gitignore

---
+++ React/01basicreact/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Youtube from "./Youtube";
+
+function App() {
+  return (
+    <>
+      <h1>Welcome Shiv in Classic react app</h1>;
+      <Youtube />
+    </>
+  );
+}
+
+export default App;
