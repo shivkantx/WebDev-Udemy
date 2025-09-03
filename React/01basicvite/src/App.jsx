4ecb5607bccac359bfcf85b4b8399b12a20e05b1
Shiv Kant <shivkant639624@gmail.com>
2025-09-04 00:01:21 +0530

    custom react

--- React/01basicvite/src/App.jsx
+++ React/01basicvite/src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import Youtube from "./youtube";
 
 function App() {
+  const username = "Shiv";
   return (
     <>
-      <h1>Welcome Shiv in Vite React project</h1>
+      <h1>vite react app{2 + 2}</h1>
+      <h1>vite react app {username}</h1>
       <Youtube />
     </>
   );
--- React/01basicvite/src/main.jsx
+++ React/01basicvite/src/main.jsx
@@ -1,9 +1,34 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import React from "react";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+    children: "Click me to visit Google",
+  },
+};
+
+function myApp() {
+  return (
+    <div>
+      <h1>Custom react app</h1>
+    </div>
+  );
+}
+
+const AnotherElement = (
+  <a href="https:google.com" target="_blank">
+    visit Google
+  </a>
 );
+
+const areactElement = React.createElement(
+  "a",
+  { href: "https:google.com", target: "_blank" },
+  "click to visit googlle"
+);
+createRoot(document.getElementById("root")).render(<App />);
