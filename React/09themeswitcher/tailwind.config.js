c9d05a88262d3bc6b95f80f2974d5249c2ad1e82
Shiv Kant <shivkant639624@gmail.com>
2025-09-08 22:56:19 +0530

    09themeswitcher for light and dark theme not working with tailwind latest version

--- React/09themeswitcher/tailwind.config.js
+++ React/09themeswitcher/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  darkMode: "class", // 👈 important for theme toggling
   plugins: [],
 };
